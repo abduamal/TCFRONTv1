8366bea59ec3a7ca42b2f6abde7e8d88bb528056
Amal Abdu <abduamal@ymail.com>
2022-01-02 02:53:27 -0600

    fetching and clean-up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import RecipeFetch from './RecipeFetch.js'
 
 function App() {
   return (
     <div className="App">
-
+      <RecipeFetch/>
     </div>
   );
 }
--- src/RecipeFetch.js
+++ src/RecipeFetch.js
@@ -1,11 +1,27 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
 
 function RecipeFetch(){
-  const {recipe, setRecipe} = useState([])
+  const [recipes, setRecipe] = useState([])
 
   useEffect(() => {
-    axios.get("http://localhost:4000/api/recipes")
+    axios.get('http://localhost:4000/api/recipes')
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.log(err);
+      })
   })
+
+  return(
+    <div>
+      <ul>
+        {recipes.map(recipe => <li key={recipe.id}>{recipe.recipe_title}</li>)}
+      </ul>
+    </div>
+  )
 }
+
+export default RecipeFetch
